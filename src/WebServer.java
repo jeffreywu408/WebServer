41e9e791875d29cca7d5c5ef1aa87475308f88d1
Jeffrey <jeffreywu408@gmail.com>
2019-02-02 11:43:58 -0800

    Update WebServer.java

--- src/WebServer.java
+++ src/WebServer.java
@@ -11,7 +11,8 @@ public class WebServer {
         File directory = new File(directoryName).getAbsoluteFile();
 
         if (directory.exists() || directory.mkdirs()) {
-            if (System.setProperty("user.dir", directory.getAbsolutePath()) != null) {
+            if (System.setProperty("user.dir", directory.getAbsolutePath())
+                    != null) {
                 return true;
             }
         }
@@ -27,22 +28,26 @@ public class WebServer {
                 // Set desired working directory
                 if (setDirectory(inputs[i + 1].replaceAll("\"", ""))) {
                     // The directory was set correctly
-                    System.out.println("Directory Successfully Set: \""
-                            + System.getProperty("user.dir").replace('\\', '/') + "/\"");
+                    System.out.println("Directory Successfully Set: "
+                            + System.getProperty("user.dir").replace('\\', '/')
+                            + "/");
                 } else {
                     // The directory was not set correctly
                     System.out.println("Error in Setting Directory");
-                    System.out
-                            .println("Using Directory \"" + System.getProperty("user.dir").replace('\\', '/') + "/\"");
+                    System.out.println("Using Directory "
+                            + System.getProperty("user.dir").replace('\\', '/')
+                            + "/");
                 }
 
             } else if (inputs[i].equalsIgnoreCase("-port")) {
                 // Set port to be used
                 try {
                     port = Integer.parseInt(inputs[i + 1]);
-                    System.out.println("Port number successfully set to " + port);
+                    System.out.println("Port number successfully set to "
+                            + port);
                 } catch (NumberFormatException ex) {
-                    System.out.println("Error parsing port number. Using port number 8080 instead.");
+                    System.out.println("Error parsing port number. " +
+                            "Using port number 8080 instead.");
                 }
             }
         }
@@ -55,14 +60,19 @@ public class WebServer {
         try {
             // Start listening on port
             serverSocket = new ServerSocket(port);
-            System.out.println("\nWeb Server running on port " + serverSocket.getLocalPort());
-            System.out.println("Working Directory: \"" + System.getProperty("user.dir").replace('\\', '/') + "/\"");
+            System.out.println("\nWeb Server running on port "
+                    + serverSocket.getLocalPort());
+
+            System.out.println("Working Directory: "
+                    + System.getProperty("user.dir").replace('\\', '/') + "/");
 
             // Server infinite loop and wait for client(s) to connect
             while (true) {
                 // accept client connection
                 Socket socket = serverSocket.accept();
-                System.out.println("Connection accepted " + socket.getInetAddress() + ":" + socket.getPort());
+                System.out.println("Connection accepted "
+                        + socket.getInetAddress() + ":"
+                        + socket.getPort());
 
                 // Create a new thread and handle the client request
                 RequestHandler handler = new RequestHandler(socket);
